5b19aef7eff49735c537104140d4799a0b44a746
wwx362192 <1273268119@qq.com>
2018-06-10 21:01:56 +0800

    Initial commit

---
+++ src/components/todoitem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li @click="handerDelet">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props:['content','index'],
+  methods:{
+    handerDelet(){
+      this.$emit('del',this.index);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/todolist.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <input v-model="inputValue" type="text">
+      <button @click="handerClick">提交按钮</button>
+     
+      <todo-item 
+        v-for="(item , index) of lists"
+        :key="index"
+        :content="item"
+        :index="index"
+        @del="handerDel"
+      ></todo-item>
+  </div>
+</template>
+
+<script>
+import todoitem from './components/todoitem'
+export default {
+  components:{
+    'todo-item':todoitem
+  },
+  data(){
+    return{
+        inputValue: "",
+        lists: []
+      }
+  },
+  methods:{
+    handerClick(){
+      this.lists.push(this.inputValue);
+      this.inputValue="";
+    },
+    handerDel(index){
+      this.lists.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
